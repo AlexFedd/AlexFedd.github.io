ccb2e76c7eacfa560d51d27157a7f28aec2e92b4
alexfedd <alexandrfedorov508@gmail.com>
2023-09-17 22:22:41 +0300

    SpruceVita is ready

--- sprucevita/assets/js/main.js
+++ sprucevita/assets/js/main.js
@@ -1,1 +1,11 @@
-import './menu-switcher.js';
+const preloaders = document.querySelectorAll('.preloader');
+
+preloaders.forEach(preloader => {
+    const preloaderPhoto = preloader.querySelector('img');
+    if(!preloaderPhoto.complete) {
+        preloaderPhoto.style.opacity = '0';
+        preloaderPhoto.addEventListener('load', event => {
+            preloaderPhoto.style.opacity ='';
+        });
+    }
+});
