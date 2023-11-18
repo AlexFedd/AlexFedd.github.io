adb8a841941424c27b3a4ef99118b5c96844072b
alexfedd <alexandrfedorov508@gmail.com>
2023-11-19 02:07:16 +0300

    MNTN Header desktop is ready

---
+++ mountains-site/assets/js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('scroll', e => {
+    Object.assign(document.documentElement, {
+        style: `--scrollTop: ${this.scrollY}px`
+    });
+});
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+ScrollSmoother.create({
+    wrapper:'.wrapper',
+    content: '.wrapper__container',
+    effects: true,
+})
