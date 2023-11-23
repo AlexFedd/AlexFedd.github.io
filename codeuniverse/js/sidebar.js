2018c965410e011964061321c13b4a84c3b77885
alexfedd <alexandrfedorov508@gmail.com>
2023-11-23 20:07:32 +0300

    sidebar

--- codeuniverse/js/sidebar.js
+++ codeuniverse/js/sidebar.js
@@ -3,19 +3,29 @@ const containerWidth = document.querySelector('.container').getBoundingClientRec
 const mainContent = document.querySelector('.main-content')
 const footer = document.querySelector('footer')
 const sidebarTop = (sidebar.getBoundingClientRect().top + document.documentElement.scrollTop).toFixed();
+let fixedMoment = 0;
 window.addEventListener('scroll', e => {
     const scrollTop = document.documentElement.scrollTop;
-console.log();
-    if(scrollTop > Number(sidebarTop) - 50) {
-        sidebar.classList.add('sidebar--fixed');
+console.log((sidebar.getBoundingClientRect().bottom), (footer.getBoundingClientRect().top - 120));
+    if((scrollTop > Number(sidebarTop) - 50) && (((sidebar.getBoundingClientRect().bottom) < (footer.getBoundingClientRect().top - 120)) || scrollTop < fixedMoment ) ) {
+        fixedMoment = 0;
         sidebar.style.marginLeft = containerWidth - 160 - sidebar.clientWidth + 'px';
+        sidebar.style.position = 'fixed';
+        sidebar.style.top = '50px';
+        sidebar.style.bottom = '';
+
         mainContent.style.marginRight = sidebar.clientWidth + 30 + 'px'
     }
-    else if ( > footer.getBoundingClientRect().top + scrollTop) {
-
+    else if (((sidebar.getBoundingClientRect().bottom) >= (footer.getBoundingClientRect().top - 120))) {
+        fixedMoment = scrollTop;
+        sidebar.style.position = 'absolute';
+        sidebar.style.bottom = '50px';
+        sidebar.style.top = '';
     }
     else {
-        sidebar.classList.remove('sidebar--fixed');
+        sidebar.style.position = '';
+        sidebar.style.bottom = '';
+        sidebar.style.top = '';
         sidebar.style.marginLeft = '';
         mainContent.style.marginRight = '';
     }
